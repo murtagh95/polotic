d82f165f8cafa780d61b95b1bce8010456635243
Catalano Nicolas <nec.catalano@gmail.com>
2021-07-06 20:22:36 -0300

    Agrego el modelo Pasajero

--- AEROLINEA/models.py
+++ AEROLINEA/models.py
@@ -27,5 +27,10 @@ class Vuelo(models.Model):
     def __str__(self):
         return f"Vuelo #{self.id}: {self.origen} hacia {self.destino}"
 
+class Pasajero(models.Model):
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=64)
+    vuelos = models.ManyToManyField(Vuelo, blank=True, related_name='pasajeros')
 
-
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"

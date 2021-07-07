63c3b797e618017ee472a6acd2a106f21951f82c
Catalano Nicolas <nec.catalano@gmail.com>
2021-07-06 21:51:43 -0300

    Creación de vista para los vuelos y para agregar pasajeros al mismo

--- AEROLINEA/urls.py
+++ AEROLINEA/urls.py
@@ -5,4 +5,6 @@ app_name = "AEROLINEA"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:vuelo_id>', views.vuelo, name='vuelo'),
+    path('<int:vuelo_id>/reserva', views.reserva, name='reserva'),
 ]
--- AEROLINEA/views.py
+++ AEROLINEA/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Vuelo
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Vuelo, Pasajero
 
 
 def index(request):
@@ -7,3 +9,27 @@ def index(request):
         'lista_vuelos': Vuelo.objects.all()
     })
 
+def vuelo(request, vuelo_id):
+    # Busco el vuelo por id
+    vuelo = Vuelo.objects.get(id=vuelo_id)
+    # Busco todos los que no son pasajeros del vuelo
+    no_son_pasajeros = Pasajero.objects.exclude(vuelos=vuelo).all()
+    # Busco los pasajeros que estan registrados a dicho vuelo
+    pasajeros = vuelo.pasajeros.all()
+    return render(request, 'vuelos/vuelo.html', {
+        "vuelo": vuelo,
+        "pasajeros": pasajeros,
+        "no_son_pasajeros": no_son_pasajeros,
+    })
+
+def reserva(request, vuelo_id):
+    if request.method == "POST": 
+        vuelo = Vuelo.objects.get(pk=vuelo_id)
+
+        pasajero_id = int(request.POST.get('pasajero'))
+
+        pasajero = Pasajero.objects.get(pk=pasajero_id)  # Busca por primary key
+
+        pasajero.vuelos.add(vuelo)  # Agrego el vuelo al pasajero
+
+        return HttpResponseRedirect(reverse("AEROLINEA:vuelo", args=(vuelo_id,)))
